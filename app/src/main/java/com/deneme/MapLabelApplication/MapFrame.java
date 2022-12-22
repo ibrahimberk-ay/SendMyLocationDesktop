e7e44cc3235de0ad61dcfa928851dcb7482dac2f
Berk <ayibrahimberk@gmail.com>
2022-12-22 17:17:17 +0300

    Latest Update - 22.12.2022

--- app/src/main/java/com/deneme/MapLabelApplication/MapFrame.java
+++ app/src/main/java/com/deneme/MapLabelApplication/MapFrame.java
@@ -12,6 +12,8 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -28,6 +30,9 @@ import org.jxmapviewer.viewer.DefaultTileFactory;
 import org.jxmapviewer.viewer.GeoPosition;
 import org.jxmapviewer.viewer.TileFactoryInfo;
 import org.jxmapviewer.viewer.WaypointPainter;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  *
@@ -37,10 +42,10 @@ public class MapFrame extends javax.swing.JFrame {
 
     //etiketleri tutan set
     private final Set<MyLabel> labels = new HashSet<>();
-    
+
     //Kullanýcý verilerini tutan kullanýcý seti
     private final List<Users> users = new ArrayList<>();
-    
+
     //Týklama eventini saðlayan deðiþken
     private EventWaypoint event;
 
@@ -56,7 +61,7 @@ public class MapFrame extends javax.swing.JFrame {
         TileFactoryInfo info = new OSMTileFactoryInfo();
         DefaultTileFactory tileFactory = new DefaultTileFactory(info);
         jXMapViewer.setTileFactory(tileFactory);
-        
+
         //Harita çaðýrýldýðýnda ilk hangi pozisyondan baþlanacaðýný ayarlýyorum
         GeoPosition geo = new GeoPosition(38.963745, 35.243322);//Türkiye
         jXMapViewer.setAddressLocation(geo);
@@ -70,6 +75,7 @@ public class MapFrame extends javax.swing.JFrame {
         event = getEvent();
 
     }
+
     //Etikete týklandýðýnda etiket bilgilerini yansýtan event
     private EventWaypoint getEvent() {
         return (MyLabel label) -> {
@@ -86,7 +92,6 @@ public class MapFrame extends javax.swing.JFrame {
             jXMapViewer.add(label.getButton());
         }
     }
-    
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -136,26 +141,26 @@ public class MapFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void addLabelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addLabelActionPerformed
-           /*Clear Labels*/
-            clearWaypoint();
+        /*Clear Labels*/
+        clearWaypoint();
 
-            /*Add Labels*/
-            createUserWithData();
-            for (Users user : users) {
-                String clickString = 
-                        "User Id: "+ user.getUserId() + "\n" + 
-                        "User Name: " + user.getUserName()+ "\n" +
-                        "Latitude: " + user.getLatitude() + "\n" +
-                        "Longitude: " + user.getLongitude() + "\n" +
-                        "Date & Time: " + user.getDateTime() + "\n" +
-                        "Condition: " + user.getCondition();
-                labels.add(new MyLabel(clickString, event, new GeoPosition(user.getLatitude(), user.getLongitude()),user.getPriority()));
-            }
-            //Kullanýcý listesini her seferinde 0'lýyorum ki her çaðýrýldýðýnda ayný kullanýcýdan defalarca çaðýrýlmasýn ve bellek kullanýmý artmasýn
-            users.clear();
-            initLabel();
+        /*Add Labels*/
+        createUserWithData();
+        for (Users user : users) {
+            String clickString
+                    = "User Id: " + user.getUserId() + "\n"
+                    + "User Name: " + user.getUserName() + "\n"
+                    + "Latitude: " + user.getLatitude() + "\n"
+                    + "Longitude: " + user.getLongitude() + "\n"
+                    + "Date & Time: " + user.getDateTime() + "\n"
+                    + "Condition: " + user.getCondition();
+            labels.add(new MyLabel(clickString, event, new GeoPosition(user.getLatitude(), user.getLongitude()), user.getPriority()));
+        }
+        //Kullanýcý listesini her seferinde 0'lýyorum ki her çaðýrýldýðýnda ayný kullanýcýdan defalarca çaðýrýlmasýn ve bellek kullanýmý artmasýn
+        users.clear();
+        initLabel();
     }//GEN-LAST:event_addLabelActionPerformed
-    
+
     private void clearWaypoint() {
         //Etiket silme fonksiyonu
         for (MyLabel lbl : labels) {
@@ -164,34 +169,56 @@ public class MapFrame extends javax.swing.JFrame {
         labels.clear();
         initLabel();
     }
+
     private void createUserWithData() {
         try {
             Firestore db = FirestoreClient.getFirestore();
             // database'den locationDataSets koleksiyonu çaðýrýyorum
             ApiFuture<QuerySnapshot> query = db.collection("locationDataSets").get();
-            
+
             // query.get() blocks on response
             QuerySnapshot querySnapshot = query.get();
-            
+
             /*Çaðýrýlan koleksiyonun dökümantasyonlarýný çaðýrýyorum bunlar bizim tanýmladýðýmýz isim,zaman,konum bilgilerini içeriyor.*/
             List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
-            
+
             //Bütün dökümanlarý tek tek çaðýrýyorum ve verileri ile yeni kullanýcýlar oluþturuyorum.
             for (QueryDocumentSnapshot document : documents) {
-                users.add(new Users(
-                        document.getId(),
-                        document.getString("userName"),
-                        document.getDouble("latitude"),
-                        document.getDouble("longitude"),
-                        document.getString("dateTime"),
-                        document.getDouble("priority").intValue(),
-                        document.getString("condition"))
-                );
+                if (checkDate(document)) {
+                    users.add(new Users(
+                            document.getId(),
+                            document.getString("userName"),
+                            document.getDouble("latitude"),
+                            document.getDouble("longitude"),
+                            document.getString("dateTime"),
+                            document.getDouble("priority").intValue(),
+                            document.getString("condition"))
+                    );
+                }
             }
         } catch (InterruptedException | ExecutionException ex) {
             Logger.getLogger(MapFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+    }
+
+    private boolean checkDate(QueryDocumentSnapshot document) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String dateTime = dtf.format(now);
+        int index = dateTime.indexOf(" ");
+        dateTime = dateTime.substring(0, index);
+        String documentDateTime = document.getString("dateTime").substring(0, index);
+        try {  
+            Date localDate = new SimpleDateFormat("yyyy/MM/dd").parse(dateTime);
+            Date documentDate = new SimpleDateFormat("yyyy/MM/dd").parse(documentDateTime);           
+            if(localDate.compareTo(documentDate) == 0){
+                return true;
+            }
+        } catch (ParseException ex) {
+            Logger.getLogger(MapFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
     public static void main(String args[]) {
